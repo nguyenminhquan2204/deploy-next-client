b210e408d3a687b992a0b609c0c166d4b775283e
Nguyễn Minh Quân <142667586+nguyenminhquan2204@users.noreply.github.com>
2026-03-06 11:47:11 +0700

    Init

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '4000'
+        // pathname: '/photos/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'api.nguyenquandev.io.vn',
+      }
+    ]
+  },
+  logging: {
+    fetches: {
+      fullUrl: true
+    }
+  }
+}
+
+export default nextConfig
