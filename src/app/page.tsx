2ddf17ad8495d679e15fce9e80fa839be1d100c8
Nguyễn Minh Quân <142667586+nguyenminhquan2204@users.noreply.github.com>
2026-03-06 17:38:20 +0700

    Test CI/CD in branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,5 +6,5 @@ export const metadata: Metadata = {
 }
 
 export default function Home() {
-  return <main>Xin chào Nguyen Minh Quan</main>
+  return <main>Xin chào Nguyen Minh Quan in enviroment develop</main>
 }
